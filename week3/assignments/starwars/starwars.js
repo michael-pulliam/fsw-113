67766d463e32d9f68a3949151970382bc64b7f4d
michael-pulliam <michael.pulliam@bryanuniversity.edu>
2021-10-19 00:09:07 -0500

    update starWars assignment

--- week3/assignments/starwars/starwars.js
+++ week3/assignments/starwars/starwars.js
@@ -1,6 +1,12 @@
 // declare any necessary variables
-
+let queryType
+let itemID
 // define a function called 'fetchData()' that passes the values from 
+function fetchData() {
+    queryType = document.querySelector("#queryType").value;
+    itemID = document.querySelector("#itemID").value;
+    getFromSWAPI();
+}
 // the 'queryType' and 'itemID' elements in starwars.html to the function 
 // called 'getFromSWAPI()'
 
@@ -16,10 +22,17 @@ function getFromSWAPI() {
     .catch(function(err) {
         console.warn(err)
     })
+   
 }
-
 // create a new function called 'updateInfo()' that receives the data from 
 // the call to that function (see above). Use logic to write the appropriate
 //labels to 'dataLabel1' and 'dataLabel2' elements in starwars.html, as well
 // as the appropriate values from the data object to the 'dataValue1' and 
 // 'dataValue2' elements in starwars.html.
+function updateInfo(data){
+    var oj = Object.keys(data);
+    document.querySelector("#dataLabel1").textContent = `${oj[0]}:`
+    document.querySelector("#dataValue1").textContent = `${data[oj[0]]}`
+    document.querySelector("#dataLabel2").textContent = `${oj[3]}:`
+    document.querySelector("#dataValue2").textContent = `${data[oj[3]]}`
+}
