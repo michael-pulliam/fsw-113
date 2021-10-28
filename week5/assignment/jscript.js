c407f57b8d169d4ecd7b3df8aecffc6dec5514d8
michael-pulliam <michael.pulliam@bryanuniversity.edu>
2021-10-28 15:31:26 -0500

    complete

--- week5/assignment/jscript.js
+++ week5/assignment/jscript.js
@@ -1,14 +1,38 @@
 // Declare any necessary variables.
-
+var score;
+var possScores;
 // Add am evemt listener that responds to the click of the "print" button by calling a function to instantiate
 //  a new student object, and another function to print the certificate.
-
+document.querySelector("#print").addEventListener("click", newStudent)
 // Add an event listener that responds to the click of the reset button by resetting all the values
 // both in the form and in the certificate.
+document.querySelector("#reset").addEventListener("click", reset);
 
+function reset(){
+    var clearForm = [...document.querySelectorAll("input")]
+        clearForm.forEach((e, i) =>(clearForm[i].value = ""))
+    var clearCert = [...document.querySelectorAll(".cert-data")]
+        clearCert.forEach((e, i) => (clearCert[i].textContent = ""))
+}
+
+function newStudent(){
+    let myStudent = new student()
+    certStudent(myStudent)
+}
 // Create a function that instantiates a new student object with the input from the HTML form.
 
 // Create a function that fills in the student's name, class name, and calculated grade on the certificate .
-
+function certStudent(info){
+    document.querySelector("#certStudentName").textContent = info.name
+    document.querySelector('#certClassName').textContent = info.course
+    document.querySelector("#certStudentName").textContent = info.name
+    info.addScores();
+    info.addPossScores();
+    info.convertScores(score, possScores);
+}
 // Create a function that converts the contents of a comma-separated text string to a numeric array.
 // You can use this function when instantiating the arrays in the student object.
+
+function myConvert(arry){
+    arry.forEach((e, i) => (arry[i] = Number(arry[i])))
+}
--- week5/assignment/student.js
+++ week5/assignment/student.js
@@ -3,3 +3,42 @@
 // - a method that adds up all the student's scores
 // - a method that adds up all the possible scores
 // - a method that calculates the student's letter grade (divide the student's score by the possible scores and use the resulting decimal to determine grade)
+
+class student{
+    constructor(){
+        this.name = document.querySelector("#studentName").value
+        this.course = document.querySelector("#className").value
+        this.scores = document.querySelector("#studentScores").value.split(",")
+        this.pScores = document.querySelector("#possibleScores").value.split(",")
+    }
+    addScores(){
+        myConvert(this.scores)
+        score = this.scores.reduce((perValue, currValue) => (perValue + currValue))
+    }
+    addPossScores(){
+        myConvert(this.pScores)
+        possScores = this.pScores.reduce((perValue, currValue) => (perValue + currValue))
+
+    }
+    convertScores(totalScore, possiScore){
+        if ((totalScore / possiScore) * 100 >= 100){
+            document.querySelector("#certGrade").textContent = "A+"
+        } else if ((totalScore / possiScore) * 100 >= 95 && (totalScore / possiScore) * 100 < 100){
+            document.querySelector("#certGrade").textContent = "A-"
+        }  else if ((totalScore / possiScore) * 100 >= 90 && (totalScore / possiScore) * 100 < 95){
+            document.querySelector("#certGrade").textContent = "B+"
+        }  else if ((totalScore / possiScore) * 100 >= 85 && (totalScore / possiScore) * 100 < 90){
+            document.querySelector("#certGrade").textContent = "B-"
+        }  else if ((totalScore / possiScore) * 100 >= 80 && (totalScore / possiScore) * 100 < 85){
+            document.querySelector("#certGrade").textContent = "C+"
+        }  else if ((totalScore / possiScore) * 100 >= 75 && (totalScore / possiScore) * 100 < 80){
+            document.querySelector("#certGrade").textContent = "C-"
+        }  else if ((totalScore / possiScore) * 100 >= 70 && (totalScore / possiScore) * 100 < 75){
+            document.querySelector("#certGrade").textContent = "D+"
+        }  else if ((totalScore / possiScore) * 100 >= 65 && (totalScore / possiScore) * 100 < 70){
+            document.querySelector("#certGrade").textContent = "D-"
+        } else {
+            document.querySelector("#certGrade").textContent = "F"
+        }
+    }
+}
